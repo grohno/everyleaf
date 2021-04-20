dafbdc9ec9f81301bf0b3789f528109aeb8c13dc
yuki ohno <jeanadler33@gmail.com>
2021-04-20 06:48:13 +0000

    検索のロジックをコントローラからモデルのスコープに移動

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,11 +6,11 @@ class TasksController < ApplicationController
       @tasks = Task.order(expired_at: :desc)
     elsif params[:search]
       if params[:search_title].present? && params[:search_status].present?
-        @tasks = Task.where("title LIKE ?", "%#{params[:search_title]}%").where(status: params[:search_status])
-      elsif params[:search_title].present? && params[:search_status].blank?
-        @tasks = Task.where("title LIKE ?", "%#{params[:search_title]}%")
-      elsif  params[:search_title].blank? && params[:search_status].present?
-        @tasks = Task.where(status: params[:search_status])
+        @tasks = Task.search_title(params[:search_title]).search_status(params[:search_status])
+      elsif params[:search_title].present?
+        @tasks = Task.search_title(params[:search_title])
+      elsif params[:search_status].present?
+        @tasks = Task.search_status(params[:search_status])
       else
         @tasks = Task.order(created_at: :desc)
       end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,6 @@ class Task < ApplicationRecord
   validates :expired_at, presence: true
   validates :status, presence: true
   enum status: { 未着手: 1, 着手: 2, 完了: 3 }
+  scope :search_title, -> (search_title) { where("title LIKE ?", "%#{search_title}%") }
+  scope :search_status, -> (search_status) { where(status: search_status)}
 end
